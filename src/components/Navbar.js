925552c85a2573c663fbb54abb4ba7422f0e08a9
Priyank Gupta <priyaaank@gmail.com>
2025-03-05 23:21:47 +0530

    Updated navbar collapse navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,12 +12,6 @@ function Navbar() {
           <Link to="/" className="brand">
             {isCollapsed ? "WT" : "WealthTracker"}
           </Link>
-          <button 
-            className="collapse-btn"
-            onClick={() => setIsCollapsed(!isCollapsed)}
-          >
-            {isCollapsed ? '→' : '←'}
-          </button>
         </div>
         <Link 
           to="/" 
@@ -59,6 +53,27 @@ function Navbar() {
           {!isCollapsed && <span>John Doe</span>}
         </div>
       </div>
+      <button 
+        className="collapse-btn"
+        onClick={() => setIsCollapsed(!isCollapsed)}
+        title={isCollapsed ? "Expand" : "Collapse"}
+      >
+        <svg 
+          width="24" 
+          height="24" 
+          viewBox="0 0 24 24" 
+          fill="none" 
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path 
+            d={isCollapsed 
+              ? "M8.59 16.59L13.17 12L8.59 7.41L10 6L16 12L10 18L8.59 16.59Z"  // Right arrow
+              : "M15.41 16.59L10.83 12L15.41 7.41L14 6L8 12L14 18L15.41 16.59Z" // Left arrow
+            }
+            fill="currentColor"
+          />
+        </svg>
+      </button>
     </nav>
   );
 }
